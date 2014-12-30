50e9c7d6b164ca1f299d3907456bed09690b2d52
anantharam <anantharam@live.com>
2014-12-30 21:19:59 +0530

    Pushing to Github

---
+++ app/src/main/java/com/anantharam/sunshine/WeatherDataParser.java
@@ -0,0 +1,29 @@
+package com.anantharam.sunshine;
+
+/**
+ * Created by anantharam on 11/12/14.
+ */
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class WeatherDataParser {
+
+    /**
+     * Given a string of the form returned by the api call:
+     * http://api.openweathermap.org/data/2.5/forecast/daily?q=94043&mode=json&units=metric&cnt=7
+     * retrieve the maximum temperature for the day indicated by dayIndex
+     * (Note: 0-indexed, so 0 would refer to the first day).
+     */
+    public static double getMaxTemperatureForDay(String weatherJsonStr, int dayIndex)
+            throws JSONException {
+
+        JSONObject weather = new JSONObject(weatherJsonStr);
+        JSONArray days = weather.getJSONArray("list");
+        JSONObject dayInfo = days.getJSONObject(dayIndex);
+        JSONObject tempInfo = dayInfo.getJSONObject("temp");
+        double temperature = tempInfo.getDouble("max");
+        return temperature;
+    }
+
+}
